bf8728d3a0433c43fec3b3f77c9a97bad71317c0
DaneHartenstine <310286@shakopeeschools.org>
2025-10-27 09:42:30 -0500

    Restrictions
    Made the turtle movement. Started on the restrictions of movement in the area.

--- 1.2.5 - Shall We Play a Game/1.2.5_Dane.py
+++ 1.2.5 - Shall We Play a Game/1.2.5_Dane.py
@@ -6,6 +6,22 @@ wn = trtl.Screen()
 
 area = trtl.Turtle()
 
+user_trtl = trtl.Turtle(shape="circle")
+user_trtl.penup()
+
+
+current_direction = ""
+
+arrow = trtl.Turtle(shape="arrow")
+arrow.hideturtle()
+arrow.penup()
+arrow.goto(300,300)
+
+middle=(0,0)
+top=(0,50)
+right=(50,0)
+bottow=(0,-50)
+left=(-50,0)
 
 
 #area for movement of user
@@ -24,9 +40,18 @@ area.hideturtle()
 
 #User Movement
 
+wn.listen()
+
 def check_key(key):
   wn.tracer(True)
-
+  if key == "Up":
+    user_trtl.goto(user_trtl.xcor(), user_trtl.ycor()+50)
+  if key == "Right":
+    user_trtl.goto(user_trtl.xcor()+50, user_trtl.ycor())
+  if key == "Down":
+    user_trtl.goto(user_trtl.xcor(), user_trtl.ycor()-50)
+  if key == "Left":
+    user_trtl.goto(user_trtl.xcor()-50, user_trtl.ycor())
 
 for key_press in {"Up", "Down", "Left", "Right"}: 
   wn.onkeypress(lambda l=key_press: check_key(l), key_press)
