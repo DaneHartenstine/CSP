0280a8f1e9b40427b037b7ecdaa80bd18490cc89
DaneHartenstine <310286@shakopeeschools.org>
2025-10-28 09:18:16 -0500

    Restriction
    i was able to finish the restriction of the user only being able to move within the plus

--- 1.2.5 - Shall We Play a Game/1.2.5_Dane.py
+++ 1.2.5 - Shall We Play a Game/1.2.5_Dane.py
@@ -4,11 +4,10 @@ import random as rand
 #Setup
 wn = trtl.Screen()
 
-area = trtl.Turtle()
+plus = trtl.Turtle()
 
 user_trtl = trtl.Turtle(shape="circle")
 user_trtl.penup()
-
 
 current_direction = ""
 
@@ -20,23 +19,23 @@ arrow.goto(300,300)
 middle=(0,0)
 top=(0,50)
 right=(50,0)
-bottow=(0,-50)
+bottom=(0,-50)
 left=(-50,0)
 
 
 #area for movement of user
 
-area.penup()
-area.goto(-25,75)
-area.pendown()
-for plus in range(4):
-  area.forward(50)
-  area.right(90)
-  area.forward(50)
-  area.left(90)
-  area.forward(50)
-  area.right(90)
-area.hideturtle()
+plus.penup()
+plus.goto(-25,75)
+plus.pendown()
+for area in range(4):
+  plus.forward(50)
+  plus.right(90)
+  plus.forward(50)
+  plus.left(90)
+  plus.forward(50)
+  plus.right(90)
+plus.hideturtle()
 
 #User Movement
 
@@ -44,17 +43,34 @@ wn.listen()
 
 def check_key(key):
   wn.tracer(True)
-  if key == "Up":
-    user_trtl.goto(user_trtl.xcor(), user_trtl.ycor()+50)
-  if key == "Right":
-    user_trtl.goto(user_trtl.xcor()+50, user_trtl.ycor())
-  if key == "Down":
-    user_trtl.goto(user_trtl.xcor(), user_trtl.ycor()-50)
-  if key == "Left":
-    user_trtl.goto(user_trtl.xcor()-50, user_trtl.ycor())
+  if user_trtl.pos() == middle:
+    if key == "Up":
+      user_trtl.goto(user_trtl.xcor(), user_trtl.ycor()+50)
+    if key == "Right":
+      user_trtl.goto(user_trtl.xcor()+50, user_trtl.ycor())
+    if key == "Down":
+      user_trtl.goto(user_trtl.xcor(), user_trtl.ycor()-50)
+    if key == "Left":
+      user_trtl.goto(user_trtl.xcor()-50, user_trtl.ycor())
+
+  if user_trtl.pos() == top:
+    if key == "Down":
+      user_trtl.goto(user_trtl.xcor(), user_trtl.ycor()-50)
+  if user_trtl.pos() == bottom:
+    if key == "Up":
+      user_trtl.goto(user_trtl.xcor(), user_trtl.ycor()+50)
+  if user_trtl.pos() == right:
+    if key == "Left":
+      user_trtl.goto(user_trtl.xcor()-50, user_trtl.ycor())
+  if user_trtl.pos() == left:
+    if key == "Right":
+      user_trtl.goto(user_trtl.xcor()+50, user_trtl.ycor())
 
 for key_press in {"Up", "Down", "Left", "Right"}: 
   wn.onkeypress(lambda l=key_press: check_key(l), key_press)
+
+
+
 
 #Random Objects
 
