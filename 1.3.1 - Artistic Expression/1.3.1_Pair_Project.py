5f34906853aff79a72d57d45dd9e5b0b0f26970d
DaneHartenstine <310286@shakopeeschools.org>
2025-11-11 09:20:00 -0600

    Update 1.3.1_Pair_Project.py

--- 1.3.1 - Artistic Expression/1.3.1_Pair_Project.py
+++ 1.3.1 - Artistic Expression/1.3.1_Pair_Project.py
@@ -1,10 +1,28 @@
 import turtle as trtl
 
 #Envelope
-
+trtl.speed(0)
+trtl.hideturtle()
+trtl.penup()
+trtl.goto(-200,100)
+trtl.pendown()
+for i in range(2):
+    trtl.forward(400)
+    trtl.right(90)
+    trtl.forward(200)
+    trtl.right(90)
+trtl.goto(0,0)
+trtl.goto(200,100)
+trtl.penup()
+trtl.goto(0,25)
+trtl.color("red")
+trtl.fillcolor("red")   
+trtl.pendown() 
+trtl.begin_fill()
+trtl.circle(-20)
+trtl.end_fill()
 
 #Ask what branch of military
-
 
 #message
 
