71d03006743507d455befc26d878e81cdba418ba
DaneHartenstine <310286@shakopeeschools.org>
2025-11-11 08:59:24 -0600

    Create 1.3.1_Pair_Project.py

---
+++ 1.3.1 - Artistic Expression/1.3.1_Pair_Project.py
@@ -0,0 +1,22 @@
+import turtle as trtl
+
+#Envelope
+
+
+#Ask what branch of military
+
+
+#message
+
+
+#Show branch of military in background
+
+
+#Eagle 
+
+
+#Flag
+
+
+wn = trtl.Screen()
+wn.mainloop()
