a0cd6fd652c3b5c6831e8dfd3f5ff7aa7a9ad905
DaneHartenstine <310286@shakopeeschools.org>
2025-11-12 10:37:17 -0600

    Update 1.3.1_Pair_Project.py
    We finished the definitions for the different branches of government, with the pictures

--- 1.3.1 - Artistic Expression/1.3.1_Pair_Project.py
+++ 1.3.1 - Artistic Expression/1.3.1_Pair_Project.py
@@ -1,32 +1,6 @@
 import turtle as trtl
 wn = trtl.Screen()
 
-#Envelope
-trtl.speed(0)
-trtl.hideturtle()
-trtl.penup()
-trtl.goto(-200,100)
-trtl.pendown()
-for i in range(2):
-    trtl.forward(400)
-    trtl.right(90)
-    trtl.forward(200)
-    trtl.right(90)
-trtl.goto(0,0)
-trtl.goto(200,100)
-trtl.penup()
-trtl.goto(0,25)
-trtl.color("red")
-trtl.fillcolor("red")   
-trtl.pendown() 
-trtl.begin_fill()
-trtl.circle(-20)
-trtl.end_fill()
-
-trtl.penup()
-trtl.color("black")
-trtl.goto(-100,150)
-trtl.write("Click me!", font=("Arial", 45, "bold"))
 
 def clear(x,y):
     trtl.clear()
@@ -36,33 +10,148 @@ wn.onclick(clear)
 
 #Ask what branch of military
 boat_image = "navy boat.gif"
+navy_image = "navy.gif"
+airforce_image = "air_force.gif"
+jet_image = "jet.gif"
+tank_image = "tank.gif"
+army_image = "army.gif"
+humvee_image = "humvee.gif"
+marine_image = "marines.gif"
+rocket_image = "rocket.gif"
+space_image = "space_force.gif"
+lighthouse_image = "lighthouse.gif"
+coastguard_image = "coast_guard.gif"
 
+#envelope
+def envelope(): 
+    trtl.pensize(5)   
+    trtl.color("black")
+    trtl.speed(0)
+    trtl.hideturtle()
+    trtl.penup()
+    trtl.goto(-200,100)
+    trtl.pendown()
+    for i in range(2):
+        trtl.forward(400)
+        trtl.right(90)
+        trtl.forward(200)
+        trtl.right(90)
+    trtl.goto(0,0)
+    trtl.goto(200,100)
+    trtl.penup()
+    trtl.goto(0,25)
+    trtl.color("red")
+    trtl.fillcolor("red")   
+    trtl.pendown() 
+    trtl.begin_fill()
+    trtl.circle(-20)
+    trtl.end_fill()
+
+    trtl.penup()
+    trtl.color("black")
+    trtl.goto(-100,150)
+    trtl.write("Click me!", font=("Arial", 45, "bold"))
+
+#Show branch of military in background
 def navy ():
     wn.addshape(boat_image)
     boat = trtl.Turtle(shape=boat_image)
-    
+    wn.addshape(navy_image)
+    navy_e = trtl.Turtle(shape=navy_image)
+    navy_e.penup()
+    navy_e.speed(0)
+    navy_e.goto(200,200)
+    trtl.color("white")
+    trtl.pensize(600)
+    trtl.pendown()
+    trtl.forward(200)
+    envelope()
 
 def airforce():
-    fasd
+    wn.addshape(airforce_image)
+    wn.addshape(jet_image)
+    jet = trtl.Turtle(shape=jet_image)
+    airforce_e = trtl.Turtle(shape=airforce_image)
+    airforce_e.penup()
+    airforce_e.speed(0)
+    airforce_e.goto(200,200)
+    trtl.color("white")
+    trtl.pensize(600)
+    trtl.pendown()
+    trtl.forward(200)
+    envelope()
+
 def army():
-    asfg
+    wn.addshape(army_image)
+    wn.addshape(tank_image)
+    tank = trtl.Turtle(shape=tank_image)
+    army_e = trtl.Turtle(shape=army_image)
+    army_e.penup()
+    army_e.speed(0)
+    army_e.goto(200,200)
+    trtl.color("white")
+    trtl.pensize(600)
+    trtl.pendown()
+    trtl.forward(200)
+    envelope()
+
 def marine():
-    afeg
+    wn.addshape(marine_image)
+    wn.addshape(humvee_image)
+    humvee = trtl.Turtle(shape=humvee_image)
+    marine_e = trtl.Turtle(shape=marine_image)
+    marine_e.penup()
+    marine_e.speed(0)
+    marine_e.goto(200,200)
+    trtl.color("white")
+    trtl.pensize(600)
+    trtl.pendown()
+    trtl.forward(200)
+    envelope()
+
 def spaceforce():
-    qwr
+    wn.addshape(space_image)
+    wn.addshape(rocket_image)
+    rocket = trtl.Turtle(shape=rocket_image)
+    space_e = trtl.Turtle(shape=space_image)
+    space_e.penup()
+    space_e.speed(0)
+    space_e.goto(-200,200)
+    trtl.color("white")
+    trtl.pensize(600)
+    trtl.pendown()
+    trtl.forward(200)
+    envelope()
+
 def coastguard():
-    wer
+    wn.addshape(coastguard_image)
+    wn.addshape(lighthouse_image)
+    lighthouse = trtl.Turtle(shape=lighthouse_image)
+    coastguard_e = trtl.Turtle(shape=coastguard_image)
+    coastguard_e.penup()
+    coastguard_e.speed(0)
+    coastguard_e.goto(200,200)
+    trtl.color("white")
+    trtl.pensize(6000)
+    trtl.pendown()
+    trtl.forward(200)
+    envelope()
 
 
-#branch_list = [navy, airforce, army, marine, spaceforce, coastguard]
+
+branch_list = [navy, airforce, army, marine, spaceforce, coastguard]
+
+army()
+
+#branch = trtl.textinput("Branch of Military","What Branch of Military Did You Serve In?(navy, airforce, army, marine, spaceforce, coastguard)")
+
+#while branch not in branch_list:
+    #trtl.textinput("Thats Not A Branch, Spelling Error","What Branch of Military Did You Serve In?")
+
+
+
 
 #message
-
-
-#Show branch of military in background
-
-
-#Eagle 
 
 
 #Flag
